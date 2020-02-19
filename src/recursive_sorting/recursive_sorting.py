c209ef4ca7d8438aa95bf21e80d290730075f246
Matthew Nelson <05nelsonm@protonmail.com>
2020-02-19 11:56:15 -0500

    fix syntatical and grammatical errors

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,6 +1,6 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arr_a, arr_b):
+    elements = len(arr_a) + len(arr_b)
     merged_arr = [0] * elements
     # TO-DO
     
@@ -19,6 +19,7 @@ def merge_in_place(arr, start, mid, end):
     # TO-DO
 
     return arr
+
 
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
